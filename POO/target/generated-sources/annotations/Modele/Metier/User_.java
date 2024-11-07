fd5b4c60892e7dfb6062d8aec5f6f538b5e61a22
Selim Ben Bouzid <benbouzid.selim@gmail.com>
2024-11-08 00:37:03 +0100

    Final commit

--- POO/target/generated-sources/annotations/Modele/Metier/User_.java
+++ POO/target/generated-sources/annotations/Modele/Metier/User_.java
@@ -3,7 +3,7 @@ package Modele.Metier;
 import javax.annotation.processing.Generated;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-10-29T22:27:54", comments="EclipseLink-2.7.12.v20230209-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-11-08T00:08:53", comments="EclipseLink-2.7.12.v20230209-rNA")
 @StaticMetamodel(User.class)
 public class User_ extends Cuenta_ {
 
